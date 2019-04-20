2038692ab5ed152f9d359dbe3b8bd5e6a0d57b7a
Abhinav Deep Singh <abhinav.ds@utexas.edu>
2019-04-19 21:23:43 -0500

    Condor compatible code. (auto restart supported
    Signed-off-by: Abhinav Deep Singh <abhinav.ds@utexas.edu>

--- scripts/train_models_gqa.sh
+++ scripts/train_models_gqa.sh
@@ -5,5 +5,6 @@
     
 echo "TRAINING MOTIFNET"
 python -m models.train_rels -m sgcls -model motifnet -order leftright -nl_obj 2 -nl_edge 4 -b 1 -clip 5 \
-    -p 100 -hidden_dim 512 -pooling_dim 4096 -lr 1e-3 -ngpu 1 -ckpt /scratch/cluster/ankgarg/gqa/temp_abhinav/checkpoints/vg-faster-rcnn.tar\
-    -save_dir /scratch/cluster/ankgarg/gqa/temp_abhinav/checkpoints/gqa -nepoch 1 -use_bias
+    -p 100 -hidden_dim 512 -pooling_dim 4096 -ngpu 1 -ckpt /scratch/cluster/ankgarg/gqa/temp_abhinav/checkpoints/motifnet_objdet/vg-faster-rcnn.tar -use_bias $@
+    
+# -save_dir /scratch/cluster/ankgarg/gqa/temp_abhinav/checkpoints/motifnet_sgcls/ -nepoch 50 -lr 1e-3
