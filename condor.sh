d34203698722c29ebc24748f8a014a2732ef8ec9
Ankur Garg <ankurgarg101@gmail.com>
2019-04-22 23:40:24 -0500

    modifies condor script

--- condor.sh
+++ condor.sh
@@ -6,16 +6,15 @@ Executable = /lusr/bin/bash
 Requirements = TARGET.GPUSlot && InMastodon
 getenv = True
 request_GPUs = 1
-request_memory = 15000
 +GPUJob = true
 Notification = complete
 Notify_user = abhinav@cs.utexas.edu
 
 
-Log = /u/abhinav/Projects/condor_gnlp/logs/motifnet_sgcls_1p.log
-Error = /u/abhinav/Projects/condor_gnlp/logs/motifnet_sgcls_1p.err
-Output = /u/abhinav/Projects/condor_gnlp/logs/motifnet_sgcls_1p.out
-Initialdir = /u/abhinav/Projects/neural-motifs
+Log = /scratch/cluster/ankgarg/gqa/temp_abhinav/motifnet_sgcls_1p.log
+Error = /scratch/cluster/ankgarg/gqa/temp_abhinavmotifnet_sgcls_1p.err
+Output = /scratch/cluster/ankgarg/gqa/temp_abhinav/motifnet_sgcls_1p.out
+Initialdir = /scratch/cluster/ankgarg/gqa/neural-motifs/
 Arguments =  scripts/train_models_gqa.sh -save_dir /scratch/cluster/ankgarg/gqa/temp_abhinav/checkpoints/motifnet_sgcls/ -nepoch 50 -lr 1e-3
 Queue 1
 
